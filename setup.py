38a4b2ffec508a07591271501d1de5c79683c2a2
Kelci Mohrman <kmohrman@nd.edu>
2023-08-21 09:22:50 -0400

    Adding setup for pip installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='topcoffea',
+    version='0.0.0',
+    description='Framework and tools that sit on top of coffea to facilitate analyses',
+    packages=setuptools.find_packages(),
+)

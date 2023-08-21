3dc061fe1d3be021f322901b0cbdc4bce6bce1c0
Kelci Mohrman <kmohrman@nd.edu>
2023-08-21 16:48:04 -0400

    Adding non py stuff to install in env

--- setup.py
+++ setup.py
@@ -5,4 +5,20 @@ setuptools.setup(
     version='0.0.0',
     description='Framework and tools that sit on top of coffea to facilitate analyses',
     packages=setuptools.find_packages(),
+    # Include data files (Note: "include_package_data=True" does not seem to work)
+    package_data={
+        "topeft" : [
+            "params/*",
+            "data/fromTTH/fakerate/*.root",
+            "data/leptonSF/*/*.root",
+            "data/leptonSF/*/*.json",
+            "data/JEC/*.txt",
+            "data/JER/*.txt",
+            "data/pileup/*.root",
+            "data/MuonScale/*txt",
+            "data/goldenJsons/*.txt",
+            "data/TauSF/*.json",
+            "data/topmva/lepid_weights/*.bin",
+        ],
+    }
 )

3e360ff07258d9f8658462ede8b63e5a920b04f4
Kelci Mohrman <kmohrman@nd.edu>
2023-08-22 17:07:54 -0400

    Update get params

--- topcoffea/modules/get_param_from_jsons.py
+++ topcoffea/modules/get_param_from_jsons.py
@@ -1,21 +1,12 @@
 import json
-from topcoffea.modules.paths import topcoffea_path
 
+class GetParam:
 
-# Return the lumi from the json/lumi.json file for a given year
-def get_lumi(year):
-    lumi_json = topcoffea_path("params/lumi.json")
-    with open(lumi_json) as f_lumi:
-        lumi = json.load(f_lumi)
-        lumi = lumi[year]
-    return lumi
+    def __init__(self,params_json_path):
+        self.__path = params_json_path
 
-
-# Retrun the param value from params.json for a given param name
-# The tc signifies that the param is in topcoffea
-def get_tc_param(param_name):
-    param_json = topcoffea_path("params/params.json")
-    with open(param_json) as f_params:
-        params = json.load(f_params)
-        param_val = params[param_name]
-    return param_val
+    def __call__(self,param_name):
+        with open(self.__path) as f_params:
+            params = json.load(f_params)
+            param_val = params[param_name]
+        return param_val

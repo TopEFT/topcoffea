8876ebdb194259707f7bcb93e5fc50b34cc7cec4
Kelci Mohrman <kmohrman@nd.edu>
2023-08-22 22:03:09 -0400

    Dont need self its not a class

--- topcoffea/modules/yield_tools.py
+++ topcoffea/modules/yield_tools.py
@@ -7,7 +7,7 @@
 #           subk : (val,err)
 #       }
 #   }
-def get_diff_between_nested_dicts(self,dict1,dict2,difftype,inpercent=False):
+def get_diff_between_nested_dicts(dict1,dict2,difftype,inpercent=False):
 
     # Get list of keys common to both dictionaries
     common_keys, d1_keys, d2_keys = self.get_common_keys(dict1,dict2)
@@ -45,7 +45,7 @@ def get_diff_between_nested_dicts(self,dict1,dict2,difftype,inpercent=False):
 #   - Checks if the differences are larger than that value
 #   - Returns False if any of the values are too large
 #   - Should a different function handle this stuff?
-def print_yld_dicts(self,ylds_dict,tag,show_errs=False,tolerance=None):
+def print_yld_dicts(ylds_dict,tag,show_errs=False,tolerance=None):
     ret = True
     print(f"\n--- {tag} ---\n")
     for proc in ylds_dict.keys():

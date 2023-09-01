872bbf5b6bd60b963e7f51c5e8aac9b91e2a4b16
Brent Yates <yates.313@osu.edu>
2023-09-01 10:53:31 -0400

    Testing pip only

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setuptools.setup(
     version='0.0.0',
     description='Framework and tools that sit on top of coffea to facilitate analyses',
     packages=setuptools.find_packages(),
+    install_requires=[
+          'coffea',
+          'numpy==1.23.5',
+          'pytest',
+    ],
     # Include data files (Note: "include_package_data=True" does not seem to work)
     package_data={
         "topcoffea" : [
